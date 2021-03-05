7e1af05b2b19300d78e15259214ad9930a5ab23f
山本 <star-dust-breaker.z@outlook.jp>
2021-03-05 15:59:49 +0900

    コミット

---
+++ MyBootGApp/src/main/java/com/kotesu/springboot/MyBootGAppApplication.java
@@ -0,0 +1,13 @@
+package com.kotesu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootGAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootGAppApplication.class, args);
+	}
+
+}
---
+++ MyBootGApp/src/test/java/com/kotesu/springboot/MyBootGAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotesu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootGAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
