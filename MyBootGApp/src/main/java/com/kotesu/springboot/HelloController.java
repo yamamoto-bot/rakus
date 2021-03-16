55aa4fcd68ad865029c3a80ec5022a2efd0d2f30
山本 <star-dust-breaker.z@outlook.jp>
2021-03-17 00:51:21 +0900

    ｐ174

--- MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
+++ MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
@@ -27,5 +27,12 @@ public class HelloController{
 			this.name = name;
 			this.value = value;
 		}
+
+		public int getId() {return id;}
+		public void setId(int id) {this.id = id;}
+		public String getName() {return name;}
+		public void setName(String name) {this.name= name;}
+		public String getValue() {return value;}
+		public void setValue(String value) {this.value=value;}
 	}
 }
