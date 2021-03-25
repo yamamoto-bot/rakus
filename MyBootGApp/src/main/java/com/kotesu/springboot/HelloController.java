99b917ac9017a5b5529684877955a76fa3b58f47
山本 <star-dust-breaker.z@outlook.jp>
2021-03-25 15:28:39 +0900

    ｐ184　繰り返しの「th:each」

--- MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
+++ MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
@@ -1,20 +1,22 @@
 package com.kotesu.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController{
 
-	@RequestMapping("/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		int m = Math.abs(month) % 12;
-		m = m == 0 ? 12 : m;
-		mav.addObject("month",m);
-		mav.addObject("check",Math.floor(m / 3));
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"taro","taro@yamada","090-999-999"});
+		data.add(new String[] {"hanako","hanako@flower","080-888-888"});
+		data.add(new String[] {"sachiko","sachiko@happy","080-888-888"});
+		mav.addObject("data",data);
 		return mav;
 	}
 
