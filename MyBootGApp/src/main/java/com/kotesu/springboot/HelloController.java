d16b23696568f50979114843a255e27af860a72e
山本 <star-dust-breaker.z@outlook.jp>
2021-03-30 15:18:04 +0900

    p189 インライン処理について

--- MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
+++ MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
@@ -3,22 +3,15 @@ package com.kotesu.springboot;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController{
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num,ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("num",num);
-		if (num >= 0) {
-		   mav.addObject("check","num >= data.size() ? 0 : num");
-		} else {
-		   mav.addObject("check","num <= data.size() * -1 ? 0 : num * -1");
-		}
 		ArrayList<DataObject> data = new ArrayList<DataObject>();
 		data.add(new DataObject(0,"taro","taro@yamada"));
 		data.add(new DataObject(1,"hanako","hanako@flower"));
