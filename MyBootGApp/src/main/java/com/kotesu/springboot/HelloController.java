f3cb08d755a8c4e8e7d2c276c0348801ab8ffce2
山本 <star-dust-breaker.z@outlook.jp>
2021-04-08 22:48:25 +0900

    p224 エンティティクラスのアノテーションについて　※ｐ231までのエラー解決

--- MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
+++ MyBootGApp/src/main/java/com/kotesu/springboot/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.kotetu.springboot.repositories.MyDataRepository;
+import com.kotesu.springboot.repositories.MyDataRepository;
 
 @Controller
 public class HelloController{
--- MyBootGApp/src/main/java/com/kotesu/springboot/repositories/MyDataRepository.java
+++ MyBootGApp/src/main/java/com/kotesu/springboot/repositories/MyDataRepository.java
@@ -1,4 +1,4 @@
-package com.kotetu.springboot.repositories;
+package com.kotesu.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.kotesu.springboot.MyData;
 
 @Repository
-public interface MyDataRepository   extends JpaRepository<MyData, Long> {
+public interface MyDataRepository  extends JpaRepository<MyData,Long> {
 
 }
